1f796bf42981dffc66269a69f99c0f0e163523ab
omerbastug <bomersami@gmail.com>
2022-03-16 10:55:27 +0100

    dao

--- index.php
+++ index.php
@@ -1,11 +1,40 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require 'vendor/autoload.php';
+require_once ('/rest/dao/dao.class.php');
 
+echo "hello world";
 
-Flight::route( '/' , function(){
-    echo 'Hello world!';
-
-});
-
-Flight::start();
+$dao = new MoviesDao();
+$op = $_REQUEST['op'];
+switch ($op) {
+    case 'insert':
+      $descriptionn = $_REQUEST['descriptionn'];
+      $created = $_REQUEST['created'];
+      $dao->add($descriptionn, $created);
+      break;
+  
+    case 'delete':
+      $id = $_REQUEST['id'];
+      $dao->delete($id);
+      echo "DELETED $id";
+      break;
+  
+    case 'update':
+      $id = $_REQUEST['id'];
+      $description = $_REQUEST['descriptionn'];
+      $created = $_REQUEST['created'];
+      $dao->update($id, $descriptionn, $created);
+      echo "UPDATED $id";
+      break;
+  
+    case 'get':
+    default:
+      $results = $dao->get_all();
+      print_r($results);
+      break;
+  }
 ?>
---
+++ rest/dao/dao.class.php
@@ -0,0 +1,57 @@
+<?php
+
+class MoviesDao {
+
+  private $conn;
+
+  public function __construct(){
+
+      $servername = "localhost";
+      $username = "newuser";
+      $password = "admin";
+      $schema = "lecture1";
+
+      $this->conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+      // set the PDO error mode to exception
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  }
+
+  /**
+  * Method used to read all todo objects from database
+  */
+  public function get_all(){
+    $stmt = $this->conn->prepare("SELECT * FROM movielist");
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  /**
+  * Method used to add todo to the database
+  */
+  public function add($descriptionn, $created){
+    $stmt = $this->conn->prepare("INSERT INTO movielist (movieName, datePublished) VALUES ( :descriptionn , :created )");
+    $stmt->execute(['descriptionn' => $descriptionn, 'created' => $created]); // Same as bindParam() / Injection prevention
+  }
+
+  /**
+  * Delete todo record from the database
+  */
+  public function delete($id){
+    $stmt = $this->conn->prepare("DELETE FROM movielist WHERE id=:id");
+    $stmt->bindParam(':id', $id); // SQL injection prevention
+    $stmt->execute();
+  }
+
+  /**
+  * Update todo record
+  */
+  public function update($id, $descriptionn, $created){
+    $stmt = $this->conn->prepare("UPDATE movielist SET movieName=:descriptionn, datePublished=:created WHERE id=:id");
+    $stmt->execute(['id' => $id, 'descriptionn' => $descriptionn, 'created' => $created]);
+  }
+
+}
+
+
+?>
