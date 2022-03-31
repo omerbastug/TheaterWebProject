4a4a152c848d8aa99377984f26e2a0a65619f86c
omerbastug <bomersami@gmail.com>
2022-03-31 13:10:59 +0300

    sql file

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 /*
-SQLyog Trial v13.1.8 (64 bit)
+SQLyog Community v13.1.7 (64 bit)
 MySQL - 10.4.22-MariaDB : Database - cinemaproject
 *********************************************************************
 */
@@ -38,15 +38,16 @@ CREATE TABLE `person` (
   `surname` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` text COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `person` */
 
 insert  into `person`(`id`,`name`,`surname`,`email`) values 
 (1,'Omer','Bastug','omer@gmail.com'),
 (2,'Yusuf','Selvi','yusuf@gmail.com'),
-(3,'Mark','White','mark@gmail.com'),
-(4,'Sarah','Clarkson','sarah@gmail.com');
+(3,'Mark','White','changed@email.com'),
+(4,'Sarah','Clarkson','sarah@gmail.com'),
+(5,'NEW','person','newperson@gmail.com');
 
 /*Table structure for table `play` */
 
@@ -59,9 +60,14 @@ CREATE TABLE `play` (
   `durationMinutes` int(11) NOT NULL,
   `asd` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `play` */
+
+insert  into `play`(`id`,`name`,`author`,`durationMinutes`,`asd`) values 
+(1,'Pamuk Prenses','Shakespeare',70,1),
+(2,'Keloglanin basi kel','Makif Ersoy',30,1),
+(3,'Nasreddin Hoca 5Head','Semseddin hoca',2,1);
 
 /*Table structure for table `roles` */
 
@@ -71,9 +77,14 @@ CREATE TABLE `roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `role` text COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `roles` */
+
+insert  into `roles`(`id`,`role`) values 
+(1,'TicketChecker'),
+(2,'Client'),
+(3,'Cleaner');
 
 /*Table structure for table `sessions` */
 
@@ -88,9 +99,14 @@ CREATE TABLE `sessions` (
   `ticketsAvailable` int(11) DEFAULT NULL,
   `play_id` int(11) NOT NULL,
   PRIMARY KEY (`id`,`time`,`theatre_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `sessions` */
+
+insert  into `sessions`(`id`,`time`,`theatre_id`,`ticketChecker`,`cleaner`,`ticketsAvailable`,`play_id`) values 
+(1,'2022-03-25 11:00:00',3,15,20,NULL,1),
+(2,'2022-03-25 12:30:30',3,1,3,74,1),
+(3,'2022-03-30 11:30:30',2,2,4,142,3);
 
 /*Table structure for table `theatre` */
 
@@ -100,24 +116,38 @@ CREATE TABLE `theatre` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `numberofrows` int(11) NOT NULL,
   `numberofcolumn` int(11) NOT NULL,
+  `totalSeats` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Data for the table `theatre` */
 
-/*Table structure for table `tickets` */
+insert  into `theatre`(`id`,`numberofrows`,`numberofcolumn`,`totalSeats`) values 
+(1,11,13,143),
+(2,11,13,143),
+(3,9,9,81),
+(4,15,20,300),
+(5,15,20,292);
 
-DROP TABLE IF EXISTS `tickets`;
+/*Table structure for table `ticketspurchased` */
 
-CREATE TABLE `tickets` (
+DROP TABLE IF EXISTS `ticketspurchased`;
+
+CREATE TABLE `ticketspurchased` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `session_id` int(11) NOT NULL,
   `seatRow` int(11) NOT NULL,
   `seatColumn` int(11) NOT NULL,
+  `personID` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*Data for the table `tickets` */
+/*Data for the table `ticketspurchased` */
+
+insert  into `ticketspurchased`(`id`,`session_id`,`seatRow`,`seatColumn`,`personID`) values 
+(1,2,1,2,4),
+(4,2,3,4,1),
+(10,3,6,7,4);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
