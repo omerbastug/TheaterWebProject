3ed047596de0ab07fdb401d98554ece59b3ae44c
omerbastug <bomersami@gmail.com>
2022-07-09 23:25:09 +0200

    path adjustment

--- rest/index.php
+++ rest/index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once '../vendor/autoload.php';
-require_once 'C:\Bitnami\wampstack-8.1.2-0\apache2\htdocs\cinemaProject\rest\routes\ApiSpec.php';
+require_once __DIR__.'\routes\ApiSpec.php';
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
