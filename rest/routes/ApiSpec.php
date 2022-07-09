d65675a4facc439d206c0658c1f5f00812e7a1a5
omerbastug <bomersami@gmail.com>
2022-07-10 00:23:54 +0200

    comma

--- rest/routes/ApiSpec.php
+++ rest/routes/ApiSpec.php
@@ -4,7 +4,7 @@
 /**
  * @OA\Info(title="Theater API Specs", version="0.2", @OA\Contact(email="omer.sami.bastug@stu.ibu.edu.ba", name="Omer Sami Bastug"))
  * @OA\OpenApi(
- *    @OA\Server(url="https://walrus-app-l6exi.ondigitalocean.app/rest", description="Development Environment" )
+ *    @OA\Server(url="https://walrus-app-l6exi.ondigitalocean.app/rest", description="Development Environment" ),
  *    @OA\Server(url="http://localhost/cinemaProject/rest", description="Prod Environment" )
  * ),
  * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authorization" )
