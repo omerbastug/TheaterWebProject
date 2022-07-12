417bd463223c754de9acf122fbc31bb37d6e8028
omerbastug <bomersami@gmail.com>
2022-07-12 17:07:22 +0200

    path correction

--- rest/routes/PlayCategoryRoutes.php
+++ rest/routes/PlayCategoryRoutes.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__.'\..\services\PlayCategoryService.php';
+require_once __DIR__.'/../services/PlayCategoryService.php';
 Flight::register('playcategorydao', 'PlayCategoryService');
 
 /**
