02feccf897f48e6ce3ff7a2aa6db961cf0b58b02
omerbastug <bomersami@gmail.com>
2022-07-13 14:48:29 +0200

    list sessions for selected play

--- js/theatreProject.js
+++ js/theatreProject.js
@@ -1,30 +1,56 @@
 var TheatreService = {
-    list : function(){
-        var html=`<div  id="titlediv" class="d-flex justify-content-between  flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    list : function(val){
+        let id
+        if(val){ 
+            id = getQuerystring("id");
+        }
+        if(id == undefined){ // list all sessions
+            var html=`<div  id="titlediv" class="d-flex justify-content-between  flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                         <h1 class="h2">Sessions</h1>
                     </div>
                     <select id="categories" onchange="listcategory(value)" class="form-select" aria-label="Default select example">
                     `;
         
-        html += "<option selected>Categories</option>"; 
-        for(let i = 0;i<Object.keys(categories).length;i++){
-            html+= `<option value="${categories[i].id}">${categories[i].name}</option>`;
+            html += "<option selected>Categories</option>"; 
+            for(let i = 0;i<Object.keys(categories).length;i++){
+                html+= `<option value="${categories[i].id}">${categories[i].name}</option>`;
+            }
+            html += `</select>
+            <div  class="list-group" id="sessions"> 
+                <div class="spinner-border" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+            </div>`
+            $("#mainbody").html(html);
+            listcategory("Categories");
+        } else { // list sessions for the selected play
+            var play = PlayService.info(id);
+            var html=`<div  id="titlediv" class="d-flex justify-content-between  flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                        <h1 class="h2">Sessions for ${play.name}</h1>
+                    </div>`;
+            for(let i=0;i<Object.keys(sessions).length;i++){
+                if(play.id == sessions[i].play_id){
+                    html += `
+                        <a  href="play.html?id=${sessions[i].play_id}&sess_date=${sessions[i].time}&t_id=${sessions[i].theatre_id}&sess_id=${sessions[i].id}" class="list-group-item list-group-item-action">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-1">${sessions[i].name}</h5>
+                                <small></small>
+                            </div>
+                            <p class="mb-1">Date : ${sessions[i].time}</p>
+                            <small>${sessions[i].durationMinutes} minutes long.</small>
+                        </a>`;
+                }
+            };
+            $("#mainbody").html(html);
         }
-        html += `</select>
-        <div  class="list-group" id="sessions"> 
-            <div class="spinner-border" role="status">
-                <span class="visually-hidden">Loading...</span>
-            </div>
-        </div>`
-        $("#mainbody").html(html);
-        listcategory("Categories");
+        
     }
 }
-function SelectSeat(row, col, session){
-    $("#tickets").html(`<h1> Selected seat: Row ${row+1} Column ${col+1} at Session number ${session}
-    <button type="button" class="btn btn-info" onclick="showPurchaseModal(${row+1},${col+1},${session})">Checkout</button>`);
-    $("#seatModal").modal("hide");
-}
+// function SelectSeat(row, col, session){
+//     $("#tickets").html(`<h1> Selected seat: Row ${row+1} Column ${col+1} at Session number ${session}
+//     <button type="button" class="btn btn-info" onclick="showPurchaseModal(${row+1},${col+1},${session})">Checkout</button>`);
+//     $("#seatModal").modal("hide");
+// }
 function listcategory(value){
     if(value == "Categories") {
         var html;
@@ -65,52 +91,52 @@ function listcategory(value){
     console.log(value);
 }
 
-function showPurchaseModal(row,col,sess){
-    $("#purchaseModal").modal("show");
-    $("#loginForm").validate({
-        submitHandler: function(form){
-            var entity = Object.fromEntries((new FormData(form)).entries());
-            var PersonName;
-            var id;
-            $.ajax({
-                url: "rest/get/isaperson/"+entity.email,
-                type: "GET",
-                beforeSend: function(xhr){
-                  xhr.setRequestHeader('Authorization', localStorage.getItem('token'));
-                },
-                success: function(sessions){
-                    PersonName = sessions[0].name;
-                    id = sessions[0].id;
-                    if(PersonName == entity.name){
-                        var ticket = {"session_id": sess,"seatRow":row,"seatColumn":col,"personID": id};
-                        $.ajax({
-                            url: 'rest/add/tickets',
-                            type: 'POST',
-                            beforeSend: function(xhr){
-                                xhr.setRequestHeader('Authorization', localStorage.getItem('token'));
-                            },
-                            sessions: JSON.stringify(ticket),
-                            contentType: "application/json",
-                            dataType: "json",
-                            success: function(result) {
-                            toastr.success("Purchased!");
-                            },
-                            error: function(XMLHttpRequest, textStatus, errorThrown) {
-                            toastr.error(XMLHttpRequest.responseJSON.message);
-                            alert("Status: " + textStatus); alert("Error: " + errorThrown);
-                            }
-                        });
-                        form.submit();
-                    }
-                },
-                error: function(XMLHttpRequest, textStatus, errorThrown) {
-                  toastr.error(XMLHttpRequest.responseJSON.message);
-                  loginService.logout();
-                }
-             });
-        }
-    });
-}
+// function showPurchaseModal(row,col,sess){
+//     $("#purchaseModal").modal("show");
+//     $("#loginForm").validate({
+//         submitHandler: function(form){
+//             var entity = Object.fromEntries((new FormData(form)).entries());
+//             var PersonName;
+//             var id;
+//             $.ajax({
+//                 url: "rest/get/isaperson/"+entity.email,
+//                 type: "GET",
+//                 beforeSend: function(xhr){
+//                   xhr.setRequestHeader('Authorization', localStorage.getItem('token'));
+//                 },
+//                 success: function(sessions){
+//                     PersonName = sessions[0].name;
+//                     id = sessions[0].id;
+//                     if(PersonName == entity.name){
+//                         var ticket = {"session_id": sess,"seatRow":row,"seatColumn":col,"personID": id};
+//                         $.ajax({
+//                             url: 'rest/add/tickets',
+//                             type: 'POST',
+//                             beforeSend: function(xhr){
+//                                 xhr.setRequestHeader('Authorization', localStorage.getItem('token'));
+//                             },
+//                             sessions: JSON.stringify(ticket),
+//                             contentType: "application/json",
+//                             dataType: "json",
+//                             success: function(result) {
+//                             toastr.success("Purchased!");
+//                             },
+//                             error: function(XMLHttpRequest, textStatus, errorThrown) {
+//                             toastr.error(XMLHttpRequest.responseJSON.message);
+//                             alert("Status: " + textStatus); alert("Error: " + errorThrown);
+//                             }
+//                         });
+//                         form.submit();
+//                     }
+//                 },
+//                 error: function(XMLHttpRequest, textStatus, errorThrown) {
+//                   toastr.error(XMLHttpRequest.responseJSON.message);
+//                   loginService.logout();
+//                 }
+//              });
+//         }
+//     });
+// }
 var plays;
 function listPlays(){
     var loading = `
