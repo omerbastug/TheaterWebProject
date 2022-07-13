b9e870131573124b6625ef7998c7041b9108db41
omerbastug <bomersami@gmail.com>
2022-07-13 13:11:40 +0200

    play cards responsive sizing

--- js/theatreProject.js
+++ js/theatreProject.js
@@ -140,7 +140,7 @@ function listPlays(){
             },
             success: function(data) {
                 plays = data;
-                html += `<div id="cardgroup" class="row row-cols-1 row-cols-md-3 g-4">`;
+                html += `<div id="cardgroup" class="row row-cols-1 row-cols-xl-5 row-cols-lg-4 row-cols-md-3 row-cols-sm-2 g-4">`;
                 for(let i = 0;i<plays.length; i++){
                     html += `
                         <div class="col">
